8c85fdf47f725e544de5c0542df08cadde9ff30d
SimonNordon4 <simon.nordon@protonmail.com>
2023-08-24 13:28:37 +1000

    2h 10m - Enemy Avoidance
    Added:
    
    - Enemies will now push away from each other when overlapping.

--- unity-architecture-prototype/Assets/Scripts/EnemyController.cs
+++ unity-architecture-prototype/Assets/Scripts/EnemyController.cs
@@ -1,12 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public Transform playerTarget;
-    public float moveSpeed = 5f;
+
     
     [Header("References")]
     public EnemyManager enemyManager;
+    
+    [Header("Movement")]
+    public Transform playerTarget;
+    public float moveSpeed = 5f;
+    public float repulsionForce = 0.5f;
 
     [Header("Health")] 
     public int currentHealth = 5;
@@ -15,19 +20,59 @@ public class EnemyController : MonoBehaviour
     public int damageAmount = 1;
     public float damageCooldown = 0.2f;
     private float _timeSinceLastDamage;
+    
+    private readonly List<Transform> _nearbyEnemies = new(4);
 
     private void Update()
     {
+        // Direction towards player.
         if (playerTarget == null) return;
         var dir =  Vector3.ProjectOnPlane(playerTarget.position - transform.position,Vector3.up).normalized;
+        
+        // Push away from close enemies.
+        // TODO: We need to make the avoidance direction STRONGER the closer the enemy is.
+        // The avoidance direction will be zero at 1m apart.
+        // We will add the avoidance directions together, and then divide by total enemies to scale it down.
+        // This means if the enemies are overlapping, avoidance will contribute 50% of the direction.
+        // This will drop off to zero based on distance.
+        var totalEnemies = _nearbyEnemies.Count;
+        var avoidanceDirection = Vector3.zero;
+        for(var i = 0; i < totalEnemies; i++)
+        {
+            var enemy = _nearbyEnemies[i];
+            if (enemy == null)
+            {
+                _nearbyEnemies.RemoveAt(i);
+                i--;  // Adjust the index to account for the removed enemy
+                totalEnemies--;  // Adjust the total count of enemies
+                continue;
+            }
+            var toEnemy = enemy.position - transform.position;
 
+            // Calculate distance and project direction on plane
+            var distance = toEnemy.magnitude;
+            var enemyDir = Vector3.ProjectOnPlane(toEnemy, Vector3.up).normalized;
+
+            // Scale the direction based on distance (closer enemies have stronger influence)
+            if (distance < 1.0f)  // We don't want to divide by zero or a negative number
+            {
+                // Using the inverse of distance to scale direction
+                var scale = 1.0f - distance;
+                avoidanceDirection -= enemyDir * scale;
+            }
+        }
+        
+        
+        var updatedDir = (dir + avoidanceDirection * repulsionForce).normalized;
+
+        // Apply direction to transform.
         if (dir.magnitude > 0)
         {
-            transform.position += dir * (Time.deltaTime * moveSpeed);
+            transform.position += updatedDir * (Time.deltaTime * moveSpeed);
             transform.rotation = Quaternion.LookRotation(dir);
         }
         
-        // for damage cooldown
+        // Track Attack cooldown.
         _timeSinceLastDamage += Time.deltaTime;
     }
 
@@ -42,7 +87,17 @@ public class EnemyController : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Add nearby enemies to list.
+        if (other.CompareTag("Enemy"))
+        {
+            if(_nearbyEnemies.Count < 4)
+                _nearbyEnemies.Add(other.transform);
+        }
+    }
+
     private void OnTriggerStay(Collider other)
     {
         // Continuously damage the player.
@@ -58,6 +113,16 @@ public class EnemyController : MonoBehaviour
                 }
 
             }
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        // Remove nearby enemies from list.
+        if (other.CompareTag("Enemy"))
+        {
+            if(_nearbyEnemies.Contains(other.transform))
+                _nearbyEnemies.Remove(other.transform);
         }
     }
 }
