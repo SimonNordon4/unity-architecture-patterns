981d0b90bf13e9021d243f23a6a166738e843448
SimonNordon4 <simon.nordon@protonmail.com>
2023-08-26 09:38:12 +1000

    7h 35m - Added Enemy Spawn Action
    Added:
    
    - Enemy Spawn Action Scriptable Object, for defining enemy spawn behavior.
    
    Changes:
    
    - Cleaned up chest item scriptable objects.
    
    Known Issues:
    
    - Enemy Spawn Manager needs to be updated to use the new Enemy Spawn Actions.

---
+++ unity-architecture-prototype/Assets/Scripts/Definitions/EnemySpawnAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Definitions
+{
+    [CreateAssetMenu(fileName = "EnemySpawnAction", menuName = "Prototype/EnemySpawnAction", order = 1)]
+    public class EnemySpawnAction : ScriptableObject
+    {
+        public GameObject enemyPrefab;
+        public int numberOfEnemiesToSpawn;
+        public int spawnChance;
+    }
+}
