746e55ddaf4221523d38cb08484c4e1c3f265bd2
SimonNordon4 <simon.nordon@protonmail.com>
2023-09-01 19:05:30 +1000

    23h 30m - Store Items.
    Added:
    
    - Added Store items for every stat.

--- unity-architecture-prototype/Assets/Scripts/Definitions/StoreItem.cs
+++ unity-architecture-prototype/Assets/Scripts/Definitions/StoreItem.cs
@@ -11,11 +11,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Item", menuName = "Prototype/Store Item")]
 public class StoreItem : ScriptableObject
 {
+    public StatType type = StatType.EnemySpawnRate;
+    
     public string name = "New Item";
     public Sprite sprite;
     public Modifier[] tierModifiers;
     public int[] pricePerTier;
     public int currentTier = 0;
+
+    private void OnValidate()
+    {
+        
+        name = type.ToString();
+        // add a space inbetween every capital letter
+        name = System.Text.RegularExpressions.Regex.Replace(name, "([a-z])([A-Z])", "$1 $2");
+        
+        foreach(var mod in tierModifiers)
+        {mod.statType = type;}
+    }
 }
 
 
