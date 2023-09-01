5e27d429f21d10e33eee68ed4ebab893b8d30732
SimonNordon4 <simon.nordon@protonmail.com>
2023-09-01 14:54:20 +1000

    22h 0m - Play Testing and Value Tweaks.

--- unity-architecture-prototype/Assets/Scripts/Core/DebugManager.cs
+++ unity-architecture-prototype/Assets/Scripts/Core/DebugManager.cs
@@ -29,5 +29,31 @@ public class DebugManager : MonoBehaviour
             // half time speed
             Time.timeScale /= 2;
         }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            // increase all stats by 10%
+            var gameManager = FindObjectOfType<GameManager>();
+            gameManager.playerMaxHealth.value *= 1.1f;
+            gameManager.playerSpeed.value *= 1.1f;
+            gameManager.pistolDamage.value *= 1.1f;
+            gameManager.pistolFireRate.value *= 1.1f;
+            gameManager.swordDamage.value *= 1.1f;
+            gameManager.swordAttackSpeed.value *= 1.1f;
+            gameManager.swordRange.value *= 1.1f;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            // increase all stats by 10%
+            var gameManager = FindObjectOfType<GameManager>();
+            gameManager.playerMaxHealth.value /= 1.1f;
+            gameManager.playerSpeed.value/= 1.1f;
+            gameManager.pistolDamage.value /= 1.1f;
+            gameManager.pistolFireRate.value /= 1.1f;
+            gameManager.swordDamage.value /= 1.1f;
+            gameManager.swordAttackSpeed.value /= 1.1f;
+            gameManager.swordRange.value /= 1.1f;
+        }
     }
 }
