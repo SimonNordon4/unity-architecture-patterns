b939ac84b47ee7f154978003e64b9eaeed0ec1e7
Simon Nordon <86404984+SimonNordon4@users.noreply.github.com>
2023-10-12 17:00:07 +1000

    19h 30m - Added Enemy Melee Attack (Incomplete)
    Changes:
    
    - Added Enemy Melee Attack, known bug however.

--- unity-architecture-classic/Assets/Scripts/Classic/Character/CharacterHealth.cs
+++ unity-architecture-classic/Assets/Scripts/Classic/Character/CharacterHealth.cs
@@ -37,6 +37,7 @@ namespace Classic.Character
 
         public void TakeDamage(int damageAmount)
         {
+            Debug.Log($"Taking {damageAmount} damage");
             // check if blocked
             damageAmount -= (int)stats.block.value;
             if (damageAmount <= 0)
---
+++ unity-architecture-classic/Assets/Scripts/Classic/Enemies/Enemy/EnemyMeleeAttack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Classic.Actor;
+using Classic.Game;
+using UnityEngine;
+
+namespace Classic.Enemies.Enemy
+{
+    public class EnemyMeleeAttack : MonoBehaviour
+    {
+        [SerializeField] private LayerMask attackLayer;
+        [SerializeField] private EnemyStats stats;
+
+        private float _timeSinceLastAttack = 0f;
+
+        private void OnEnable()
+        {
+            // Preload the first attack.
+            _timeSinceLastAttack = stats.attackSpeed;
+        }
+
+        private void Update()
+        {
+            _timeSinceLastAttack += GameTime.deltaTime;
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            // doing 1/attack speed inverts it. So an attack speed of 10 = 10 attacks per second.
+            if(_timeSinceLastAttack < 1 / stats.attackSpeed) return;
+            
+            if (attackLayer == (attackLayer | (1 << other.gameObject.layer)))
+            {
+                Debug.Log("Attacking");
+                if (!other.TryGetComponent<DamageReceiver>(out var damageReceiver)) return;
+            
+                Debug.Log("Dealing damage: " + stats.damage);
+                Debug.Log("Damage object: " + damageReceiver.gameObject.name);
+                damageReceiver.TakeDamage(stats.damage);
+                _timeSinceLastAttack = 0f;
+            }
+        }
+    }
+}
