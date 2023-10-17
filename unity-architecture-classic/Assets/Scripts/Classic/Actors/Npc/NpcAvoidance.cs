b72575ad342985d61d92427a09e443a5b3f273ef
Simon Nordon <86404984+SimonNordon4@users.noreply.github.com>
2023-10-17 13:20:56 +1000

    24h 10m - Started NpcAvoidance

---
+++ unity-architecture-classic/Assets/Scripts/Classic/Actors/Npc/NpcAvoidance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Classic.Actors.Npc
+{
+    public class NpcAvoidance : ActorComponent
+    {
+        private readonly List<Collider> _neighbours = new(4);
+
+        private void OnTriggerEnter(Collider other)
+        {
+            
+            if(other.gameObject.layer != this.gameObject.layer) return;
+
+            if (_neighbours.Count >= 5) return;
+            _neighbours.Add(other);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if(other.gameObject.layer != this.gameObject.layer) return;
+            if(_neighbours.Contains(other))
+                _neighbours.Remove(other);
+        }
+    }
+}
