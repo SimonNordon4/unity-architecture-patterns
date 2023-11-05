0c17126e0d44ff7c94ed97907c4a449e0cc29d5a
SimonNordon4 <simon.nordon@protonmail.com>
2023-11-05 17:24:59 +1000

    29h 10m - Added Character Input using new input system.

---
+++ unity-architecture-classic/Assets/Scripts/Classic/Character/ApplyCharacterInput.cs
@@ -0,0 +1,58 @@
+﻿using Classic.Actors;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Classic.Character
+{
+    public class ApplyCharacterInput : ActorComponent
+    {
+        [SerializeField] private ActorMovement movement;
+        [SerializeField] private InputActionAsset actionMap;
+        [SerializeField] private ActorStats stats;
+        
+        private Stat _speedStat;
+        private InputAction _movementAction;
+        private Vector3 _lastDirection;
+
+        private void OnEnable()
+        {
+            _movementAction = actionMap.FindAction("Direction");
+            _movementAction.performed += OnMovementPerformed;
+            _movementAction.canceled += OnMovementCanceled;
+            _movementAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _movementAction.performed -= OnMovementPerformed;
+            _movementAction.canceled -= OnMovementCanceled;
+            _movementAction.Disable();
+        }
+
+        private void Start()
+        {
+            _speedStat = stats.Map[StatType.MoveSpeed];
+        }
+
+        private void OnMovementPerformed(InputAction.CallbackContext context)
+        {
+            var movementInput = context.ReadValue<Vector2>();
+            var direction = new Vector3(movementInput.x, 0, movementInput.y);
+            if (direction != _lastDirection)
+            {
+                _lastDirection = direction;
+            }
+        }
+
+        private void OnMovementCanceled(InputAction.CallbackContext context)
+        {
+            _lastDirection = Vector3.zero;
+        }
+
+        private void Update()
+        {
+            movement.SetVelocity(_lastDirection * _speedStat.value);
+            movement.SetLookDirection(_lastDirection);
+        }
+    }
+}
