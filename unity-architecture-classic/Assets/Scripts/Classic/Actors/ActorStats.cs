76c0911317edd633b781a7c5dc9c32a8fb2d74e4
SimonNordon4 <simon.nordon@protonmail.com>
2023-11-13 22:07:30 +1000

    29h 20m - Added custom editor for stats.

--- unity-architecture-classic/Assets/Scripts/Classic/Actors/ActorStats.cs
+++ unity-architecture-classic/Assets/Scripts/Classic/Actors/ActorStats.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Classic.Actors
 {
@@ -25,4 +28,38 @@ namespace Classic.Actors
             }
         }
     }
+    
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(ActorStats))]
+    public class ActorStatsEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            // Draw the default inspector
+            base.OnInspectorGUI();
+
+            // Get the target object
+            ActorStats actorStats = (ActorStats)target;
+
+            if (actorStats.Map == null) return;
+
+            foreach (var keyValuePair in actorStats.Map)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(keyValuePair.Key.ToString());
+
+                // Assuming Stat has a property InitialValue of type int
+                int newInitialValue = EditorGUILayout.IntField((int)keyValuePair.Value.initialValue);
+                if (Math.Abs(newInitialValue - keyValuePair.Value.initialValue) > 0.01f)
+                {
+                    keyValuePair.Value.initialValue = newInitialValue;
+                    EditorUtility.SetDirty(target); // Mark the object as dirty to enable saving the changed value
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+    }
+#endif
 }
