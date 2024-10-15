2dbaba4fce64bf791d858f0904b67ddff86fb036
SimonNordon4 <simon.nordon@protonmail.com>
2024-10-15 22:13:53 +1000

    Resetting Main Project

--- unity-architecture-spaghetti/Assets/Scripts/Definitions/StoreItemConfig.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StoreItemConfig", menuName = "Prototype/StoreItemConfig", order = 1)]
-public class StoreItemConfig : ScriptableObject
-{
-    public List<StoreItemDefinition> storeItemDefinitions = new();
-}
