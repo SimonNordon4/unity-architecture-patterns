0cd413182f0258dcede7ab0eb7afcc789681adba
SimonNordon4 <simon.nordon@protonmail.com>
2024-11-01 20:00:02 +1000

    7h 50m - Cleaning up game manager

--- Assets/Unity Architecture 01 - Spaghetti Pattern/Scripts/Core/GameManager.cs
+++ Assets/Unity Architecture 01 - Spaghetti Pattern/Scripts/Core/GameManager.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
 using Random = UnityEngine.Random;
 
