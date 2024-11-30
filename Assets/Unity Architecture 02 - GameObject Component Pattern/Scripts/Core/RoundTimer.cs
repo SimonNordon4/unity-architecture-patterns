286af510b3c806a979ac3ce8e898763e6f7fb012
Nordon, Simon (Energy, Pullenvale) <simon.nordon@protonmail.com>
2024-11-30 20:36:57 +1000

    5h 25m - Little fixes to help the flow.

--- Assets/Unity Architecture 02 - GameObject Component Pattern/Scripts/Core/RoundTimer.cs
+++ Assets/Unity Architecture 02 - GameObject Component Pattern/Scripts/Core/RoundTimer.cs
@@ -5,18 +5,18 @@ namespace UnityArchitecture.GameObjectComponentPattern
     public class RoundTimer : MonoBehaviour
     {
         [field:SerializeField]
-        public float RoundTime {get;private set;} = 0f;
+        public float CurrentTime {get;private set;} = 0f;
         private bool _isTimerPaused = false;
 
         private void Update()
         {
             if (_isTimerPaused) return;
-            RoundTime += Time.deltaTime;
+            CurrentTime += Time.deltaTime;
         }
 
         public void Reset()
         {
-            RoundTime = 0f;
+            CurrentTime = 0f;
         }
     }
 }
--- Assets/Unity Architecture 02 - GameObject Component Pattern/Scripts/UI/UIHud.cs
+++ Assets/Unity Architecture 02 - GameObject Component Pattern/Scripts/UI/UIHud.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 namespace UnityArchitecture.GameObjectComponentPattern
 {
