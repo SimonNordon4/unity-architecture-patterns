6807c17d220e83a3727a4754d1d3897fa5f47885
Simon Nordon <simon.nordon@protonmail.com>
2024-12-08 10:23:56 +1000

    2h 50m - Added Base SO Behaviours.

---
+++ Assets/Unity Architecture 03 - Scriptable Object Pattern/Scripts/DNA/ScriptableBehaviour.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.LowLevel;
+
+namespace UnityArchitecture.ScriptableObjectPattern
+{
+
+    public abstract class ScriptableBehaviour : ScriptableData
+    {
+        private bool _isActive;
+
+        /// <summary>
+        /// Update logic to be implemented by derived classes.
+        /// </summary>
+        public abstract void OnUpdate();
+
+        private void OnEnable()
+        {
+            base.OnEnable();
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+#endif
+        }
+
+        private void OnDisable()
+        {
+            base.OnDisable();
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+#endif
+            if (_isActive)
+            {
+                DisablePlayerLoop();
+            }
+        }
+
+        private void OnPlayModeStateChanged(PlayModeStateChange stateChange)
+        {
+            if (stateChange == PlayModeStateChange.EnteredPlayMode)
+            {
+                EnablePlayerLoop();
+            }
+            else if (stateChange == PlayModeStateChange.ExitingPlayMode)
+            {
+                DisablePlayerLoop();
+            }
+        }
+
+        private void EnablePlayerLoop()
+        {
+            if (_isActive) return;
+
+            PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+            PlayerLoopSystem updateSystem = new PlayerLoopSystem
+            {
+                type = typeof(ScriptableBehaviour),
+                updateDelegate = () =>
+                {
+                    if (Application.isPlaying) OnUpdate();
+                }
+            };
+
+            var systems = new System.Collections.Generic.List<PlayerLoopSystem>(playerLoop.subSystemList)
+            {
+                updateSystem
+            };
+            playerLoop.subSystemList = systems.ToArray();
+            PlayerLoop.SetPlayerLoop(playerLoop);
+
+            _isActive = true;
+        }
+
+        private void DisablePlayerLoop()
+        {
+            if (!_isActive) return;
+
+            PlayerLoopSystem playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+            playerLoop.subSystemList = Array.FindAll(playerLoop.subSystemList,
+                system => system.type != typeof(ScriptableBehaviour));
+            PlayerLoop.SetPlayerLoop(playerLoop);
+
+            _isActive = false;
+        }
+    }
+}
---
+++ Assets/Unity Architecture 03 - Scriptable Object Pattern/Scripts/DNA/ScriptableData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityArchitecture.ScriptableObjectPattern
+{
+    public abstract class ScriptableData : ScriptableObject
+        {
+        private bool _isPlayMode;
+
+        /// <summary>
+        /// Method to define how data should reset.
+        /// Override this in derived classes.
+        /// </summary>
+        public abstract void ResetData();
+
+        protected void OnEnable()
+        {
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+#endif
+        }
+
+        protected void OnDisable()
+        {
+#if UNITY_EDITOR
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+#endif
+        }
+
+        private void OnPlayModeStateChanged(PlayModeStateChange stateChange)
+        {
+            if (stateChange == PlayModeStateChange.EnteredPlayMode)
+            {
+                _isPlayMode = true;
+            }
+            else if (_isPlayMode && stateChange == PlayModeStateChange.ExitingPlayMode)
+            {
+                ResetData();
+                _isPlayMode = false;
+            }
+        }
+    }
+}
